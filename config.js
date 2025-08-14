91b371be3a317f71b2c7944efcb525b9c051cf5a
LinexEPA <tc000013787@gmail.com>
2025-08-14 23:49:20 +0800

    config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwzsKRenxKAXw9uqB1EpOBa1THa1vRVvM1o1UQORTxk4Lmj277jiaLviUMBlolI4YfF1A/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwzsKRenxKAXw9uqB1EpOBa1THa1vRVvM1o1UQORTxk4Lmj277jiaLviUMBlolI4YfF1A/exec'
