66117309b22292acadec393d94d75a831643edd9
LinexEPA <tc000013787@gmail.com>
2025-08-14 22:55:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // https://script.google.com/macros/s/AKfycbyzb8Mhigz023I4JBNpY7yY1Ad_xaJu_Uu1P3VfyNgsVy0B87TD-6CtSkLir63OpxzRxQ/exec
-const GAS_URL = 'https://script.google.com/macros/s/XXXXXXXXXXXX/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbyzb8Mhigz023I4JBNpY7yY1Ad_xaJu_Uu1P3VfyNgsVy0B87TD-6CtSkLir63OpxzRxQ/exec';
